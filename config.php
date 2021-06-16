ba2ed6471bf3101ebab61cf6f1e12831eccb0201
Keith798 <79943434+Keith798@users.noreply.github.com>
2021-06-16 05:35:13 +0000

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 // Database Details
 
-define( "DB_HOST", "localhost" );
-define( "DB_USER", "root" );
-define( "DB_PASSWORD", "" );
-define( "DB_NAME", "pms" );
+define( "DB_HOST", "remotemysql.com" );
+define( "DB_USER", "DBHNUPHiLf" );
+define( "DB_PASSWORD", "DwHx3AJFwa" );
+define( "DB_NAME", "DBHNUPHiLf" );
